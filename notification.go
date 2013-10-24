e278962754d8884a7f413ceca79d12af0c85ffc8
Alan Harris <ah.anachronistic@gmail.com>
2013-10-23 23:10:19 -0400

    Add a notification type and supporting tests.

---
+++ notification.go
@@ -0,0 +1,41 @@
+package gcm
+
+// The Notification type wraps the payload and the
+// registration IDs that should be recipients of the
+// message data. Access is hidden away through methods.
+type Notification struct {
+	registrationIDs []string
+	payload         map[string]interface{}
+}
+
+// Constructor.
+func NewNotification() (n *Notification) {
+	n = new(Notification)
+	n.payload = make(map[string]interface{})
+	return
+}
+
+// Returns the current payload map.
+func (this *Notification) Payload() map[string]interface{} {
+	return this.payload
+}
+
+// Get a value from the payload using a string as the key.
+func (this *Notification) Get(key string) interface{} {
+	return this.payload[key]
+}
+
+// Set a value in the payload using a string as the key.
+func (this *Notification) Set(key string, value interface{}) {
+	this.payload[key] = value
+}
+
+// Return the set of recipients of this message.
+func (this *Notification) Recipients() []string {
+	return this.registrationIDs
+}
+
+// Add a recipient to the message.
+func (this *Notification) AddRecipient(registrationID string) {
+	this.registrationIDs = append(this.registrationIDs, registrationID)
+}
---
+++ notification_test.go
@@ -0,0 +1,28 @@
+package gcm
+
+import (
+	"testing"
+)
+
+func TestAddingRecipients(t *testing.T) {
+	n := NewNotification()
+	n.AddRecipient("DEVICE_ONE")
+	if len(n.Recipients()) != 1 {
+		t.Error("expected 1 recipient; got", len(n.Recipients()))
+	}
+	n.AddRecipient("DEVICE_TWO")
+	if len(n.Recipients()) != 2 {
+		t.Error("expected 2 recipients; got", len(n.Recipients()))
+	}
+}
+
+func TestSettingAndGettingPayloads(t *testing.T) {
+	n := NewNotification()
+	n.Set("foo", "bar")
+	if n.Get("foo") != "bar" {
+		t.Error("expected to set 'foo' to 'bar'; got", n.Get("foo"))
+	}
+	if len(n.Payload()) != 1 {
+		t.Error("expected payload to have 1 value; got", len(n.Payload()))
+	}
+}
