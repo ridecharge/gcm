842e525d8adae69dc5ae043fd436ee8d69f07f77
Alan Harris <ah.anachronistic@gmail.com>
2013-10-24 00:39:54 -0400

    Add a send method to the client that accepts a notification and submits to GCM.
    Sample code I just used to send a notification to my Droid:
    
      c := gcm.NewClient("YOUR_API_KEY")
      n := gcm.NewNotification()
      n.AddRecipient("YOUR_REGISTRATION_ID")
      log.Print(c.Send(n))

--- client.go
+++ client.go
@@ -1,5 +1,11 @@
 package gcm
 
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 // The Android gateway is constant regardless of your
 // environment (in contrast to Apple, which has a sandbox
 // endpoint). However, the field is exposed in the Client
@@ -19,4 +25,19 @@ func NewClient(apiKey string) (c *Client) {
 	c.Gateway = GCM_GATEWAY
 	c.ApiKey = apiKey
 	return
+}
+
+// Sends a notification to the GCM gateway.
+func (this *Client) Send(n *Notification) string {
+	client := new(http.Client)
+	req, _ := http.NewRequest("POST", this.Gateway, strings.NewReader(n.ToJSON()))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "key="+this.ApiKey)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return err.Error()
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body)
 }
